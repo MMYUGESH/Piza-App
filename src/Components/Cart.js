218b78605578509ae6e54972f1711b4326d97b14
MMYUGESH <mmyugesh@gmail.com>
2021-04-24 22:39:34 +0530

    2 files modified

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 
 function Cart(){
+
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://pizzaappbackend.herokuapp.com/allmenu").then(res => {
+      setProduct(res.data);
+      console.log(res.data);
+    });
+  }, []);
 return(
 <div>
 <h1>Cart</h1>
+      <div>
+        {product.map(x => (
+          <div className="order">
+            <span  key={x._id}>Details</span>
+            <br />
+            <span  key={x._id}> Name:{x.name}</span>
+            <br />
+            <span  key={x._id}> size:{x.size}</span>
+            <br />
+            <p>Price Details</p>
+            <span  key={x._id}>price:{x.price}</span>
+            <br />
+            <span  key={x._id}> Total Price:{(x.price)*300}</span>
+            <br />
+           
+        
+          </div>
+        ))}
+      </div>
+
+
 
 </div>
 
--- src/Components/Orders.js
+++ src/Components/Orders.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function Orders(){
-return(
-<div>
-<h1>Orders</h1>
+function Orders() {
+  const [product, setProduct] = useState([]);
 
-</div>
-
-)
+  useEffect(() => {
+    axios.get("https://pizaorders.herokuapp.com/orders").then(res => {
+      setProduct(res.data);
+      console.log(res.data);
+    });
+  }, []);
+  console.log(setProduct)
+  return (
+     <div>
+      <div>
+        {product.map(x => (
+          <div className="order" key={x}>
+            <span>Details</span>
+            <br />
+            <span> Address:{x.adress}</span>
+            <br />
+            <span> Name:{x.name}</span>
+            <br />
+            
+            <span> Phone Number:{x.phone}</span>
+            <br />
+            <p/>
+            <span>Custom Pizza</span>
+            <br />
+            <span>Base:{x.base}</span>
+            <br />
+            <span>Cheese:{x.cheese}</span>
+            <br />
+            <span>Meat:{x.meat}</span>
+            <br />
+            <span>Sauce:{x.sauce}</span>
+            <br />
+            <span>veggie:{x.veggie}</span>
+            <br />
+            <p>Price Details</p>
+            <span>Number of Pizza:{x.qty}</span>
+            <br />
+            <span> Total Price:{(x.qty)*300}</span>
+            <br />
+           
+        
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Orders;
